e17e41699f9e83aea36349b367b0dd76bb318296
ThibautFgi <thibaut.fgi@hotmail.com>
2024-06-04 14:30:08 +0200

    mapbox v2 button center paris

---
+++ Map/script.js
@@ -0,0 +1,26 @@
+// Remplace 'YOUR_MAPBOX_ACCESS_TOKEN' par ton propre token d'accès Mapbox
+mapboxgl.accessToken = 'pk.eyJ1IjoianVzdGFjaGlwcyIsImEiOiJjbHdxcGQ2ajUwNDNkMmxzZTFiODdwZzV6In0._cOwcGPG1sDGSf25DKU5Ig';
+
+const parisCoordinates = [2.3522, 48.8566]; // Coordonnées de Paris
+
+// Initialisation de la carte
+const map = new mapboxgl.Map({
+    container: 'map', // ID de l'élément HTML où la carte sera rendue
+    style: 'mapbox://styles/mapbox/streets-v11', // Style de la carte
+    center: parisCoordinates, // Centre initial de la carte
+    zoom: 12 // Niveau de zoom initial
+});
+
+// Ajout des contrôles de navigation (zoom et rotation)
+map.addControl(new mapboxgl.NavigationControl());
+
+// Fonction pour recentrer la carte sur Paris
+function centerMapOnParis() {
+    map.flyTo({
+        center: parisCoordinates,
+        essential: true // Ce paramètre garantit l'utilisation de l'animation essentielle pour une meilleure expérience utilisateur
+    });
+}
+
+// Ajout d'un écouteur d'événement pour le bouton
+document.getElementById('centerButton').addEventListener('click', centerMapOnParis);
