48603471d1d34b86fadf46c15fd4f03739dafd3c
ThibautFgi <thibaut.fgi@hotmail.com>
2024-06-06 11:40:13 +0200

    sql fix delete on cascade add

--- BDD_MJ/lancement.sql
+++ BDD_MJ/lancement.sql
@@ -1,55 +1,187 @@
--- Création de la base de données
-CREATE DATABASE IF NOT EXISTS MountainJourney;
-USE MountainJourney;
+-- Utilisation de la base de données
+-- USE MountainJourney;
 
--- Création de la table Utilisateur
-CREATE TABLE IF NOT EXISTS Utilisateur (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    mot_de_passe VARCHAR(255) NOT NULL,
-    date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- Création des tables
+
+-- !!!!!!!!!!!!!!!!!!!!!!!
+-- Table "users"
+-- !!!!!!!!!!!!!!!!!!!!!!!
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE users (
+    User_Id int NOT NULL AUTO_INCREMENT,
+    User_FirstName VARCHAR(80),
+    User_LastName VARCHAR(80),
+    User_Phone INTEGER,
+    User_Email VARCHAR(80) NOT NULL,
+    User_Password VARCHAR(80) NOT NULL,
+    PRIMARY KEY (`User_Id`)
 );
 
--- Création de la table Parcours
-CREATE TABLE IF NOT EXISTS Parcours (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titre VARCHAR(255) NOT NULL,
-    description TEXT,
-    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    utilisateur_id INT,
-    latitude_depart DECIMAL(10, 8),
-    longitude_depart DECIMAL(11, 8),
-    distance DECIMAL(5, 2),
-    duree_estime TIME,
-    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateur(id) ON DELETE CASCADE
+LOCK TABLES `users` WRITE;
+/* INSERT INTO `users` VALUES (); */
+UNLOCK TABLES;
+
+-- !!!!!!!!!!!!!!!!!!!!!!!
+-- Table "maps"
+-- !!!!!!!!!!!!!!!!!!!!!!!
+
+DROP TABLE IF EXISTS `maps`;
+
+CREATE TABLE maps (
+    Map_Id int NOT NULL AUTO_INCREMENT,
+    User_Id INTEGER NOT NULL,
+    Map_Name VARCHAR(80) NOT NULL,
+    Map_Description VARCHAR(120),
+    Map_LikeNumber INTEGER,
+    Map_NumberCommentary INTEGER,
+    Map_TravelTime FLOAT,
+    Map_TotalDistance FLOAT,
+    Map_Image VARCHAR(80),
+    Map_Rating INTEGER,
+    PRIMARY KEY (`Map_Id`),
+    FOREIGN KEY (User_Id) REFERENCES users(User_Id)
+        ON DELETE CASCADE
 );
 
--- Création de la table Commentaire
-CREATE TABLE IF NOT EXISTS Commentaire (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    contenu TEXT NOT NULL,
-    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    utilisateur_id INT,
-    parcours_id INT,
-    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateur(id) ON DELETE CASCADE,
-    FOREIGN KEY (parcours_id) REFERENCES Parcours(id) ON DELETE CASCADE
+LOCK TABLES `maps` WRITE;
+/* INSERT INTO `maps` VALUES (); */
+UNLOCK TABLES;
+
+-- !!!!!!!!!!!!!!!!!!!!!!!
+-- Table "marks"
+-- !!!!!!!!!!!!!!!!!!!!!!!
+
+DROP TABLE IF EXISTS `marks`;
+
+CREATE TABLE marks (
+    Mark_Id int NOT NULL AUTO_INCREMENT,
+    Map_Id INTEGER NOT NULL,
+    Mark_Description VARCHAR(120),
+    Mark_Name VARCHAR(80),
+    Mark_Latitude FLOAT,
+    Mark_Longitude FLOAT,
+    PRIMARY KEY (`Mark_Id`),
+    FOREIGN KEY (Map_Id) REFERENCES maps(Map_Id)
+        ON DELETE CASCADE
 );
 
--- Création de la table Favori
-CREATE TABLE IF NOT EXISTS Favori (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    utilisateur_id INT,
-    parcours_id INT,
-    date_ajout TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateur(id) ON DELETE CASCADE,
-    FOREIGN KEY (parcours_id) REFERENCES Parcours(id) ON DELETE CASCADE
+LOCK TABLES `marks` WRITE;
+/* INSERT INTO `marks` VALUES (); */
+UNLOCK TABLES;
+
+-- !!!!!!!!!!!!!!!!!!!!!!!
+-- Table "routes"
+-- !!!!!!!!!!!!!!!!!!!!!!!
+
+DROP TABLE IF EXISTS `routes`;
+
+CREATE TABLE routes (
+    Route_Id int NOT NULL AUTO_INCREMENT,
+    Mark_Start INTEGER NOT NULL,
+    Mark_End INTEGER NOT NULL,
+    Route_Name VARCHAR(80) NOT NULL,
+    Route_Description VARCHAR(120),
+    Route_Distance FLOAT,
+    PRIMARY KEY (`Route_Id`),
+    FOREIGN KEY (Mark_Start) REFERENCES marks(Mark_Id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (Mark_End) REFERENCES marks(Mark_Id)
+        ON DELETE CASCADE
 );
 
--- Création d'un utilisateur pour l'API
-CREATE USER 'api_user'@'%' IDENTIFIED BY 'securepassword';
-GRANT SELECT, INSERT, UPDATE, DELETE ON MountainJourney.* TO 'api_user'@'%';
+LOCK TABLES `routes` WRITE;
+/* INSERT INTO `routes` VALUES (); */
+UNLOCK TABLES;
+
+-- !!!!!!!!!!!!!!!!!!!!!!!
+-- Table "likes"
+-- !!!!!!!!!!!!!!!!!!!!!!!
+
+DROP TABLE IF EXISTS `likes`;
+
+CREATE TABLE `likes` (
+    User_Id INTEGER NOT NULL,
+    Map_Id INTEGER NOT NULL,
+    PRIMARY KEY (User_Id, Map_Id),
+    FOREIGN KEY (User_Id) REFERENCES users(User_Id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (Map_Id) REFERENCES maps(Map_Id)
+        ON DELETE CASCADE
+);
+
+LOCK TABLES `likes` WRITE;
+/* INSERT INTO `likes` VALUES (); */
+UNLOCK TABLES;
+
+-- !!!!!!!!!!!!!!!!!!!!!!!
+-- Table "comments"
+-- !!!!!!!!!!!!!!!!!!!!!!!
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE comments (
+    Comment_Id int NOT NULL AUTO_INCREMENT,
+    User_Id INTEGER NOT NULL,
+    Map_Id INTEGER NOT NULL,
+    Comment_Content VARCHAR(120) NOT NULL,
+    Comment_Date VARCHAR(80) NOT NULL,
+    PRIMARY KEY (`Comment_Id`),
+    FOREIGN KEY (User_Id) REFERENCES users(User_Id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (Map_Id) REFERENCES maps(Map_Id)
+        ON DELETE CASCADE
+);
+
+LOCK TABLES `comments` WRITE;
+/* INSERT INTO `comments` VALUES (); */
+UNLOCK TABLES;
+
+-- !!!!!!!!!!!!!!!!!!!!!!!
+-- Table "friendlists"
+-- !!!!!!!!!!!!!!!!!!!!!!!
+
+DROP TABLE IF EXISTS `friendlists`;
+
+CREATE TABLE friendlists (
+    User_Main_Id INTEGER NOT NULL,
+    User_Id INTEGER NOT NULL,
+    PRIMARY KEY (User_Main_Id, User_Id),
+    FOREIGN KEY (User_Main_Id) REFERENCES users(User_Id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (User_Id) REFERENCES users(User_Id)
+        ON DELETE CASCADE
+);
+
+LOCK TABLES `friendlists` WRITE;
+/* INSERT INTO `friendlists` VALUES (); */
+UNLOCK TABLES;
+
+-- !!!!!!!!!!!!!!!!!!!!!!!
+-- Table "tokens"
+-- !!!!!!!!!!!!!!!!!!!!!!!
+
+DROP TABLE IF EXISTS `tokens`;
+
+CREATE TABLE tokens (
+    Token_Id int NOT NULL AUTO_INCREMENT,
+    User_Id INTEGER NOT NULL,
+    Token_Value VARCHAR(80),
+    PRIMARY KEY (`Token_Id`),
+    FOREIGN KEY (User_Id) REFERENCES users(User_Id)
+        ON DELETE CASCADE
+);
+
+LOCK TABLES `tokens` WRITE;
+/* INSERT INTO `tokens` VALUES (); */
+UNLOCK TABLES;
+
+-- Création d'un utilisateur pour l'API avec des privilèges limités
+CREATE USER 'api'@'%' IDENTIFIED BY 'Prout123';
+
+-- Accorder des privilèges de lecture et écriture sur toutes les tables
+GRANT SELECT, INSERT, UPDATE, DELETE ON MountainJourney.* TO 'api'@'%';
+
+-- Appliquer les modifications
 FLUSH PRIVILEGES;
-
--- Insérer des données de test
-INSERT INTO Utilisateur (nom, email, mot_de_passe) VALUES ('Admin', 'admin@mountainjourney.com', 'adminpassword');
--- TODO.md
+++ TODO.md
@@ -12,6 +12,9 @@
 
 
 test map (evalution de la faisibiliter technique une aprem midi) 28/05
+premier jet map avec interaction ui = button, center to paris
+mise en place du dmc
+
 
 
 
