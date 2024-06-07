486501f68812f9fc79b77cfaffa0816ba4dddc74
ThibautFgi <thibaut.fgi@hotmail.com>
2024-06-07 15:11:57 +0200

    reorganistation

--- SITE_MJ/API_MJ/API/Models/Friendlists.cs
+++
@@ -1,8 +0,0 @@
-namespace Models
-{
-    public class Friendlist
-    {
-        public int User_Main_Id { get; set; }
-        public int User_Id { get; set; }
-    }
-}
--- SITE_MJ/API_MJ/API/Models/Likes.cs
+++
@@ -1,8 +0,0 @@
-namespace Models
-{
-    public class Likes
-    {
-        public int User_Id { get; set; }
-        public int Map_Id { get; set; }
-    }
-}
