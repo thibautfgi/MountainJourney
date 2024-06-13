0e321e506eb2e019220c6bc7160eeac7c19ed2bc
ThibautFgi <thibaut.fgi@hotmail.com>
2024-06-13 23:07:32 +0200

    degage api test

--- API_TEST/Models/Friendlists.cs
+++
@@ -1,9 +0,0 @@
-namespace Models
-{
-    public class Friendlists
-    {
-        public int Friendlist_Id { get; set; }
-        public int User_Main_Id { get; set; }
-        public int User_Id { get; set; }
-    }
-}
--- API_TEST/Models/Likes.cs
+++
@@ -1,9 +0,0 @@
-namespace Models
-{
-    public class Likes
-    {
-        public int Like_Id {get; set; }
-        public int User_Id { get; set; }
-        public int Map_Id { get; set; }
-    }
-}
